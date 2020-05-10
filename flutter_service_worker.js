6d2edffe4cf81f840b6c816a085f0d3055a9c6fe
Cyril Dubos <58974447+cyrildubos@users.noreply.github.com>
2020-05-10 16:56:37 +0200

    New build

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -1,17 +1,20 @@
 'use strict';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
-"assets/FontManifest.json": "01700ba55b08a6141f33e168c4a6c22f",
+  "assets/AssetManifest.json": "801125fca546f506babac9af4ec061ba",
+"assets/FontManifest.json": "18eda8e36dfa64f14878d07846d6e17f",
 "assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
-"assets/LICENSE": "0000d255865246c9b55862d96ffd3089",
+"assets/LICENSE": "59844a026d05c04ff832a90250bb1baa",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
+"assets/packages/font_awesome_flutter/lib/fonts/fa-brands-400.ttf": "5a37ae808cf9f652198acde612b5328d",
+"assets/packages/font_awesome_flutter/lib/fonts/fa-regular-400.ttf": "2bca5ec802e40d3f4b60343e346cedde",
+"assets/packages/font_awesome_flutter/lib/fonts/fa-solid-900.ttf": "2aa350bd2aeab88b601a593f793734c0",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "index.html": "7fb9a8f1915acbf8d52e4fa74ff427cf",
 "/": "7fb9a8f1915acbf8d52e4fa74ff427cf",
-"main.dart.js": "0e852fb4bfff9965a085a5fc9c392006",
+"main.dart.js": "4feedaea975fa918733e7b0524ac40d0",
 "manifest.json": "ed9804d454e85f44791a1f885170f008"
 };
 
