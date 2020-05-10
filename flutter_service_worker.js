0fc8320a55fb32d8fc073ec6ef46e798a9f9427d
Cyril Dubos <58974447+cyrildubos@users.noreply.github.com>
2020-05-10 17:21:36 +0200

    New build

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -12,9 +12,9 @@ const RESOURCES = {
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "7fb9a8f1915acbf8d52e4fa74ff427cf",
-"/": "7fb9a8f1915acbf8d52e4fa74ff427cf",
-"main.dart.js": "4feedaea975fa918733e7b0524ac40d0",
+"index.html": "cabb1adbcbedf1dc3ebbd896e02ee1be",
+"/": "cabb1adbcbedf1dc3ebbd896e02ee1be",
+"main.dart.js": "c45c649ab9c33d5c6e1f966ecca5bf1d",
 "manifest.json": "ed9804d454e85f44791a1f885170f008"
 };
 
